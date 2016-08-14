Fixed bug in softmax E_Frac evaluation

NN/nneval.py
@@ -8,23 +8,22 @@ from NN import Collections
 from NN.nntest import nntest
 import numpy as np
 def nneval(nn,loss,train_x,train_y,val_x,val_y,epochNumber):
-    # NEEDS FIXING
+    
     nn.Testing = 1
     # Training performance
     nn = nnff.nnff(nn,train_x,train_y)
     loss.Training.E[epochNumber] = nn.L
-    
+    # Validation performance
     if val_x is not None and val_y is not None:
          nn = nnff.nnff(nn,val_x,val_y)
          loss.Validity.E[epochNumber] = nn.L
-    else:
-        nn.Testing = 0
-        #Calculate mis-classifcation rate if softmax
-        if nn.Output == "softmax":
-            er_train,dummy = nntest(nn,train_x,train_y)
-            loss.Training.E[epochNumber] = er_train 
-        
-            if val_x is not None and val_y is not None:
-                er_val,dummy = nntest(nn,train_x,train_y)
-                loss.Validity.E[epochNumber] = er_val; 
+    nn.Testing = 0
+    #Calculate mis-classifcation rate if softmax
+    if nn.Output == "softmax":
+        er_train,dummy = nntest(nn,train_x,train_y)
+        loss.Training.E_Frac[epochNumber] = er_train 
+ 
+        if val_x is not None and val_y is not None:
+            er_val,dummy = nntest(nn,val_x,val_y)
+            loss.Validity.E_Frac[epochNumber] = er_val; 
     return loss