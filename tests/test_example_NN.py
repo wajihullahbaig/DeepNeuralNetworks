a9c56ac463274e8d552d1fa6c59f2f6af8e26ffa
wajihullahbaig <wajihullahbaig@gmail.com>
2016-08-16 21:17:34 +0500

    Tested plotting once again. Not what we have in matlab but shows the plot good enough

--- tests/test_example_NN.py
+++ tests/test_example_NN.py
@@ -37,36 +37,38 @@ def test_example_NN():
     nn = FFBPNN.NN(np.array([784, 100 ,10]))
     # numepochs = Number of full sweep through data
     # batchsize = Mean gradient step over this many samples
-    options = {"numepochs":1,"batchsize":100,"plot":1}
-    nn,L = nntrain.nntrain(nn,train_x,train_y,options)
+    options = {"numepochs":2,"batchsize":100,"plot":1}
+    nn,L = nntrain.nntrain(nn,train_x,train_y,options,1)
     er,bad = nntest.nntest(nn,test_x,test_y)
     assert (er < 0.08),"Too big error"
+    del options
     del nn
-    
+     
     # ex2 neural net with L2 weight decay
     np.random.seed(1) # Setting the random seed so that the weights are generated same as in matlab code - default is twister algorithm            
     nn = FFBPNN.NN(np.array([784, 100 ,10]))
     nn.WeightPenaltyL2 = 1e-4
-    nn,L = nntrain.nntrain(nn,train_x,train_y,options)
+    options = {"numepochs":1,"batchsize":100,"plot":1}    
+    nn,L = nntrain.nntrain(nn,train_x,train_y,options,2)
     er,bad = nntest.nntest(nn,test_x,test_y)
     assert (er < 0.08),"Too big error"
     del nn
-    
+     
     # ex3 nerual net with dropout    
     np.random.seed(1) # Setting the random seed so that the weights are generated same as in matlab code - default is twister algorithm            
     nn = FFBPNN.NN(np.array([784, 100 ,10]))
     nn.DropoutFraction = 0.5
-    nn,L = nntrain.nntrain(nn,train_x,train_y,options)
+    nn,L = nntrain.nntrain(nn,train_x,train_y,options,3)
     er,bad = nntest.nntest(nn,test_x,test_y)
     assert (er < 0.1),"Too big error"
     del nn
-    
+     
     # ex4 nerual net with sigmoid activation function    
     np.random.seed(1) # Setting the random seed so that the weights are generated same as in matlab code - default is twister algorithm            
     nn = FFBPNN.NN(np.array([784, 100 ,10]))
     nn.ActivationFunction = "sigm"
     nn.LearningRate = 1
-    nn,L = nntrain.nntrain(nn,train_x,train_y,options)
+    nn,L = nntrain.nntrain(nn,train_x,train_y,options,4)
     er,bad = nntest.nntest(nn,test_x,test_y)
     assert (er < 0.1),"Too big error"
     del nn    
@@ -75,11 +77,12 @@ def test_example_NN():
     np.random.seed(1) # Setting the random seed so that the weights are generated same as in matlab code - default is twister algorithm            
     nn = FFBPNN.NN(np.array([784, 100 ,10]))
     nn.Output = "softmax" #Use the softmax output
-    options = {"numepochs":1,"batchsize":1000,"plot":1}    
-    nn,L = nntrain.nntrain(nn,train_x,train_y,options)
+    options = {"numepochs":5,"batchsize":1000,"plot":1}    
+    nn,L = nntrain.nntrain(nn,train_x,train_y,options,5)
     er,bad = nntest.nntest(nn,test_x,test_y)
     assert (er < 0.1),"Too big error"
     del nn
-    print ("test_example_NN completed")        
+    print ("test_example_NN completed")
+    input("Press [enter] to continue.")          
 test_example_NN()      
     
