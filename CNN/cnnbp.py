e9ffb74c5f32bdc547d7654e6783c0c14d44c8df
wajihullahbaig <wajihullahbaig@gmail.com>
2016-09-16 18:17:17 +0500

    Fixed bug. Mutliplcation replaced by division

--- CNN/cnnbp.py
+++ CNN/cnnbp.py
@@ -34,15 +34,16 @@ def cnnbp(net,y):
         startIndex = j * fvnum
         endIndex = (j+1)*fvnum
         block = net.fVD[startIndex:endIndex,:]
-        net.layers[n-1].D[j] = np.reshape(block, newshape =(sa[0],sa[1], sa[2]))
+        net.layers[n-1].D[j] = np.reshape(block, newshape =(sa[0],sa[1], sa[2]),order = "F")
     
     for l in range(n-2,-1,-1):
         if isinstance(net.layers[l], Layers.ConvolutionalLayer):
             for j in range(0,len(net.layers[l].A)):
-                t1 = net.layers[l].A[j] * (1-net.layers[l].A[j])
+                t1 = net.layers[l].A[j] * (1.0-net.layers[l].A[j])
+                # The next two lines with np.repeat produce the Matlab's "expand" function
                 t2 = np.repeat(net.layers[l + 1].D[j], net.layers[l + 1].Scale,0)
                 t2 = np.repeat(t2, net.layers[l + 1].Scale,1)
-                net.layers[l].D[j] = t1*t2*(net.layers[l + 1].Scale*net.layers[l + 1].Scale)
+                net.layers[l].D[j] = t1*t2/(net.layers[l + 1].Scale*net.layers[l + 1].Scale)
         elif isinstance(net.layers[l], Layers.ScaleLayer):
             for i in range(0,len(net.layers[l].A)):
                 z = np.zeros(net.layers[l].A[0].shape)
@@ -64,10 +65,10 @@ def cnnbp(net,y):
                     temp = flipall.flipall(net.layers[l-1].A[i])
                     net.layers[l].dK[i,j] =fftconvolve(temp,net.layers[l].D[j],mode='valid')
                     # Remove the singleton dimension
-                    net.layers[l].dK[i,j] = np.squeeze(net.layers[l].dK[i,j])                    
+                    net.layers[l].dK[i,j] = np.squeeze(net.layers[l].dK[i,j])/ net.layers[l].D[j].shape[2]                    
                 net.layers[l].dB[j] = np.sum(net.layers[l].D[j])/net.layers[l].D[j].shape[2]    
     
-    net.dffW = np.dot(net.oD,net.FV.T) / net.oD.shape[1]
+    net.dffW = np.dot(net.oD,net.fV.T) / net.oD.shape[1]
     net.dffB = np.mean(net.oD,1)    
     net.dffB = net.dffB[:,None] # Adding singleton dimension for dimensionality consistency  
     return net
