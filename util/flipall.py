8fb1d440e807b411397c4ed6d7ec8b8cbb3fcd20
wajihullahbaig <wajihullahbaig@gmail.com>
2016-09-25 00:35:57 +0500

    Add missing file

---
+++ util/flipall.py
@@ -0,0 +1,15 @@
+'''
+Created on Sep 14, 2016
+
+@author: Wajih-PC
+'''
+
+import numpy as np
+def flipall(input):
+    inputCopy = input.copy()
+    return inputCopy[::-1,::-1,::-1]
+        
+        
+    
+        
+    
