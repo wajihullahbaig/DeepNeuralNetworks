2141b039436a8d5c3bec699a23718769dffc3dc3
wajihullahbaig <wajihullahbaig@gmail.com>
2016-09-25 00:35:24 +0500

    Fixed a bug -

--- util/sigmrnd.py
+++ util/sigmrnd.py
@@ -5,14 +5,14 @@ Created on Sep 24, 2016
 '''
 
 import numpy as np
+from scipy.special import erfinv 
 def sigmrnd(input):
     # Declaring variables as np float type to avoid Overflow warnings
     minusone = np.float(-1.0) 
     plusone = np.float(1.0)
-    sigmVals = np.add(plusone,np.exp(np.multiply(minusone,input)))
-    temp = np.random.uniform(0,1,input.shape)
-    truthTable = (sigmVals>temp).astype(int).astype(float)    
-    sigmVals = np.true_divide(plusone,(truthTable))    
-    return sigmVals
+    sigmVals = np.true_divide(plusone,np.add(plusone,np.exp(np.multiply(minusone,input))))
+    samples = np.random.uniform(0,1,input.shape)
+    samples= np.where(sigmVals>samples,1,0)
+    return samples
        
     
