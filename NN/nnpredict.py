e67d9d4e7e94262ba92c94ec7b35b2b87fd48e35
wajihullahbaig <wajihullahbaig@gmail.com>
2016-09-25 16:09:37 +0500

    superficial changes, 1 to true, 0 to false

--- NN/nnpredict.py
+++ NN/nnpredict.py
@@ -8,9 +8,9 @@ import operator as op
 from NN import nnff
 def nnpredict(nn,x):
     # NEEDS FIXING
-    nn.Testing = 1
+    nn.Testing = True
     nn = nnff.nnff(nn,x,np.zeros(shape = (np.shape(x)[0],nn.Size[-1])))
-    nn.Testing = 0
+    nn.Testing = False
     # max of rows for the last layer (output layer) This is where we get our predicted outputs
     labels = nn.A[-1].argmax(axis=1)
     dummy = np.amax(nn.A[-1],axis = 1);
